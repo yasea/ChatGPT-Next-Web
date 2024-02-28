ecd95cc0cfd9f7f91ce5ed33da7d507b4a526e8c
yasea <39082458+yasea@users.noreply.github.com>
2024-02-28 15:16:12 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -215,12 +215,7 @@ export function SideBar(props: { className?: string }) {
             <Link to={Path.Settings}>
               <IconButton icon={<SettingsIcon />} shadow />
             </Link>
-          </div>
-          <div className={styles["sidebar-action"]}>
-            <a href={REPO_URL} target="_blank" rel="noopener noreferrer">
-              <IconButton icon={<GithubIcon />} shadow />
-            </a>
-          </div>
+          </div> 
         </div>
         <div>
           <IconButton
